54b417847f73ac5b431ace3092d774b292dc80fd
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-09-12 16:57:00 -0400

    Fix typo in Learn React link

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Quick
+          Learn React Quickly
         </a>
       </header>
     </div>
