6cc1b4d7e57b407eadaf7693bf16b23d4ed05039
Somesh Bachani <somesh.bachani.0512@gmail.com>
2024-10-27 02:42:46 -0400

    Refactor Lab4 component to add new features and improve code structure

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,31 @@
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import EventObject from "./EventObject";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+
 export default function Lab4() {
-    return (
-        <div id = "wd-lab4">
-            <h3>Lab 4</h3>
-        </div>
-    );
+    function sayHello() {
+        alert("Hello World!");
     }
+  return (
+    <div id="wd-lab4">
+      <h3>Lab 4</h3>
+        <ClickEvent />
+        <PassingDataOnEvent />
+        <PassingFunctions theFunction={sayHello} />
+        <EventObject />
+        <Counter />
+        <BooleanStateVariables />
+        <StringStateVariables />
+        <DateStateVariable />
+        <ObjectStateVariable />
+        <ArrayStateVariable />
+    </div>
+  );
+}
